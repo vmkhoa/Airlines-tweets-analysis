d032b4c23f0ef745ff70ac8e6b3d2eea7aef3d85
Khoa Van <van.kh@northeastern.edu>
2025-06-07 13:13:53 -0400

    Restructure

--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import re
 from tqdm import tqdm
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 tqdm.pandas()
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
 
 def load_csv(file_path: str) -> pd.DataFrame:
     """Load a CSV file into a Pandas dataframe."""
@@ -21,4 +31,19 @@ def clean_text(text: str) -> str:
     text = re.sub(r"http\S+|www.\S+", "", text)  # remove URLs
     text = re.sub(r"[^a-zA-Z\s]", "", text)      # remove punctuation/numbers
     text = re.sub(r"\s+", " ", text).strip()     # remove extra spaces
-    return text
+    tokens = nltk.word_tokenize(text)            #tokenize
+    #Remove stopwords and lemmatize
+    tokens = [
+        lemmatizer.lemmatize(token) 
+        for token in tokens if token not in stop_words and len(token) > 2
+        ]
+    return ' '.join(tokens)
+
+
+def preprocess(df: pd.DataFrame, text_column: str) -> pd.DataFrame:
+    """Apply cleaning to a specified text column with progress bar."""
+    if text_column not in df.columns:
+        raise ValueError(f"'{text_column}' column not found in DataFrame.")
+    print(f"Cleaning text column: '{text_column}'")
+    df[text_column] = df[text_column].astype(str).progress_apply(clean_text)
+    return df
